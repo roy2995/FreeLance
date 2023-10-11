71818cbb94037cbe5afb0c40f439b91f6e7e56c5
Roderick Esquivel <rodericke@ilikeit.app>
2023-10-11 16:33:39 -0500

    Editor almost End

--- Assets/Scripts/Editor/DatacenterEditor.cs
+++ Assets/Scripts/Editor/DatacenterEditor.cs
@@ -14,16 +14,53 @@ public class DatacenterEditor : OdinMenuEditorWindow
     }
 
     private CreateNewMob newMob;
+    private CreateNewTurret newTurret;
+
+    protected override void OnDestroy()
+    {
+        base.OnDestroy();
+
+        if (newMob != null)
+            DestroyImmediate(newMob.mobData);
+
+        if (newTurret != null)
+            DestroyImmediate(newTurret.turretData);
+    }
 
     protected override OdinMenuTree BuildMenuTree()
     {
         var tree = new OdinMenuTree();
-        tree.Add("Create New", new CreateNewMob());
-        tree.AddAllAssetsAtPath("NewMob", "Assets/Scripts/Enemy/Objects", typeof(CreatMob));
+        
+        //Button to create a new Mob
+        tree.Add("Create New Mob", newMob);
 
+        //button to create a new Turret
+        tree.Add("Create New Turret", newTurret);
+        
+        //Files of Mobs Storage
+        tree.AddAllAssetsAtPath("Mobs Stats", "Assets/Scripts/Enemy/Objects", typeof(CreatMob));
+
+        //Files of Turrets Storage
+        tree.AddAllAssetsAtPath("Turrets Stats", "Assets/Scripts/Turrets/Objects", typeof(CreatTurret));
 
         return tree;
     }
+
+    /*protected override void OnBeginDrawEditors()
+    {
+        OdinMenuTreeSelection selected = this.MenuTree.Selection;
+
+        SirenixEditorGUI.EndHorizontalToolbar();
+        {
+            GUILayout.FlexibleSpace();
+
+            if(SirenixEditorGUI.ToolbarButton("Delete Current"))
+            {
+                CreatMob asset = selected.SelectedValue as CreatMob;
+                //string path = asset.
+            }
+        }
+    }*/
 
     public class CreateNewMob
     {
@@ -39,11 +76,37 @@ public class DatacenterEditor : OdinMenuEditorWindow
         [Button("Add New Mob")]
         private void NewMob()
         {
-            AssetDatabase.CreateAsset(mobData, "Assets/Scripts/Enemy/Objects/" + mobData.enemyName);
+            string assetPath = "Assets/Scripts/Enemy/Objects/" + mobData.enemyName + ".asset";
+            AssetDatabase.CreateAsset(mobData, assetPath);
             AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh(); // Refresh the asset database to see changes in the Editor.
 
             mobData = ScriptableObject.CreateInstance<CreatMob>();
             mobData.enemyName = "New Mob Data";
+        }
+    }
+
+    public class CreateNewTurret
+    {
+        public CreateNewTurret()
+        {
+            turretData = ScriptableObject.CreateInstance<CreatTurret>();
+            turretData.turretName = "New Turret Data";
+        }
+
+        [InlineEditor(objectFieldMode: InlineEditorObjectFieldModes.Hidden)]
+        public CreatTurret turretData;
+
+        [Button("Add New Turret")]
+        private void NewTurret()
+        {
+            string assetPath = "Assets/Scripts/Turrets/Objects/" + turretData.turretName + ".asset";
+            AssetDatabase.CreateAsset(turretData, assetPath);
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh(); // Refresh the asset database to see changes in the Editor.
+
+            turretData = ScriptableObject.CreateInstance<CreatTurret>();
+            turretData.turretName = "New Turret Data";
         }
     }
 }
